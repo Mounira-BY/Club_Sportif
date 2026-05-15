dde3bf0bb8623a38c9d5674991fceba9b4a08064
yasmine khouini <khouiniyasmine@gmail.com>
2026-05-16 00:08:47 +0100

    fixed reservations.php cors

--- shared-backend/api/reservations.php
+++ shared-backend/api/reservations.php
@@ -1,5 +1,9 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 require_once __DIR__ . '/../db.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
