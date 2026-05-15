963b7de92b50638440684d2fcd4df3f859980ea5
yasmine <maazounyasmine2003@gmail.com>
2026-05-15 22:30:06 +0100

    add config file

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'club_sportif');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_CHARSET', 'utf8mb4');
+
+define('BASE_URL', 'http://localhost/JSproject/Club_Sportif');
+define('ADMIN_URL', BASE_URL . '/admin-dashboard');
+define('API_URL',   BASE_URL . '/shared-backend/api');
+?>
